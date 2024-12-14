301deeb76c56a3cf804e9e7b3065663ad2e27f3d
hs0820 <gustn1029@gmail.com>
2024-12-14 17:20:21 +0900

    헤더 create 링크 추가

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,8 +3,11 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="max-w-[1260px] w-full m-auto px-[30px]">
+    <header className="flex max-w-[1260px] w-full m-auto px-[30px] justify-between py-[20px]">
       <Link href={"/"}>book store</Link>
+      <nav>
+        <Link href={"/books/create"} className="text-gray-500 hover:text-black transition-all">Create</Link>
+      </nav>
     </header>
   );
 };
