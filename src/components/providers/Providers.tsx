bc0ec31367c019bba62ca7dfa31c4a8e1a6b9a07
hs0820 <gustn1029@gmail.com>
2024-12-14 17:19:58 +0900

    리액트 쿼리 provider 수정

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,13 +1,9 @@
-"use client";
-
+import { queryClient } from "@/utils/http";
 import {
-  QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-import { useState } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
       {children}
